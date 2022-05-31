496008acb2017ca358867dbc770400905a8221ac
Pol136 <polina20050326@gmail.com>
2022-05-31 12:33:50 +0300

    saved

--- app/src/main/java/com/example/proect/MainActivity.java
+++ app/src/main/java/com/example/proect/MainActivity.java
@@ -1,22 +1,30 @@
 package com.example.proect;
+import android.content.SharedPreferences;
 import android.view.View;
 import android.view.View.OnClickListener;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
 import java.text.BreakIterator;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    private SharedPreferences sPref;
+    public EditText Edit;
+
+    final String SAVED_TEXT = "saved_text";
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText Edit = (EditText) findViewById(R.id.Edit);
+        Edit = (EditText) findViewById(R.id.Edit);
         Button bt = findViewById(R.id.button);
         bt.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -31,11 +39,27 @@ public class MainActivity extends AppCompatActivity {
                 transaction.commit();
             }
         });
+        Button sox = findViewById(R.id.button3);
+        sox.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                saveText();
+            }
+        });
     }
 
+    public void saveText(){
+        sPref = getSharedPreferences("MyPref", MODE_PRIVATE);
+        SharedPreferences.Editor ed = sPref.edit();
+        ed.putString(SAVED_TEXT, Edit.getText().toString());
+        ed.commit();
+        Toast.makeText(MainActivity.this, "Сохранено!", Toast.LENGTH_SHORT).show();
+    }
 
-
-
-
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        saveText();
+    }
 
 }
